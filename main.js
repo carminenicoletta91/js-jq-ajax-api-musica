2fc7745f3a32beb50216bfcec29e02fe8d2761af
carminenicoletta91 <carminenicolettakr91@gmail.com>
2019-07-30 15:21:35 +0200

    --creata chiamata ajax ad una api per inserire dei dischi in un div contenitore tramite l'utilizzo di handlebars--

---
+++ main.js
@@ -0,0 +1,27 @@
+// Attraverso una chiamata ajax all’Api di boolean avremo a disposizione
+// una decina di dischi musicali.Servendoci di handlebars stampiamo
+// tutto a schermo.In questo momento non è importante la parte grafica.
+$(document).ready(function() {
+  // creo variabile che corrisponde a ciò che mi ritorna la mia api
+  var disco;
+  $.ajax({//chiamata ajax alla mia api
+      url:"https://flynn.boolean.careers/exercises/api/array/music",
+      method:"GET",
+      success:function(data){//in caso di successo
+        disco = (data.response);
+        console.log(disco);
+        var source = $("#template").html();
+        console.log(source);
+        var template = Handlebars.compile(source);
+        // creo ciclo for per stampare tutti i cd presenti nella mia api
+        for(var i = 0 ; i < disco.length ; i++){
+          var context = {image:disco[i].poster , title:disco[i].title , autor:disco[i].author , year:disco[i].year};
+          var html = template(context);
+          $(".cds-container.container").append(html);
+        }//chiusura ciclo for
+      },//chiusura funzione success
+      error : function (richiesta,stato,error) {//in caso di errore nella chiamata
+        alert("E' avvenuto un errore. "+error);
+      }//chiusura funzione errore
+    });//chiusura chiamata ajax
+});
